57ab672e0492c98401fe4b3ef3168ee6f61a734f
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-12-13 09:51:01 +0100

    Create maven project

---
+++ src/main/java/de/allianz/1312nowy/1312nowyApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.1312nowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 1312nowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(1312nowyApplication.class, args);
+	}
+
+}
